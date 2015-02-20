9645573269e54ab5d204eb1bdb689a1e98e7683f
dance2die <dontbothermewithspam@gmail.com>
2015-02-19 21:55:37 -0500

    Initial Import

---
+++ Demo.MalApi/Demo.MalApi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Demo.MalApi.Startup))]
+namespace Demo.MalApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
